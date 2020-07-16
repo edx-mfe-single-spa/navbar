182154242f00cd80cccc30ea1b5980a81207542f
Adam Stankiewicz <agstanki@gmail.com>
2020-07-15 20:44:01 -0400

    pushing some navarz

--- src/root.component.js
+++ src/root.component.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link } from "@reach/router";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 
 export default function Root(props) {
   return (
     <header>
-      <Navbar>
+      <Navbar bg="light" expand="lg">
         <Container>
-          <Link to="/search">Find a Course</Link>
+          <Navbar.Brand as={Link} to="/">
+            edX
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/search">
+                Find a Course
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
